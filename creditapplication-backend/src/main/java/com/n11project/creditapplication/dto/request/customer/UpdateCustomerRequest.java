56e5fc03268d65c2896844bedc46dbb9145a6bd8
sametkesici <kesicisamet@hotmail.com>
2022-01-23 19:44:28 +0300

    add @notnull to dto

--- creditapplication-backend/src/main/java/com/n11project/creditapplication/dto/request/customer/UpdateCustomerRequest.java
+++ creditapplication-backend/src/main/java/com/n11project/creditapplication/dto/request/customer/UpdateCustomerRequest.java
@@ -1,15 +1,20 @@
 package com.n11project.creditapplication.dto.request.customer;
 
 import lombok.Data;
+
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
 public class UpdateCustomerRequest {
 
+    @NotNull
     private BigDecimal monthlyIncome;
 
+    @NotNull
     private String phoneNumber;
 
+    @NotNull
     private BigDecimal assurance;
 
 }
